84a4c54e7ba0dcf28a6c32a753f5822d6bbd1ed8
AkaneKross <maveraguilera@gmail.com>
2022-10-20 14:52:30 -0300

    clase 1 firebase

--- app/src/main/java/com/example/otraapp/MainActivity.java
+++ app/src/main/java/com/example/otraapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.otraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAgregar = (Button) findViewById(R.id.btnAgregar);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "HOLA XD", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
